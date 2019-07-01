56a0bc312bf03b42342600ea3d3db2a98c9527e9
NovoseletskiyVadim <novoseletskiy.v@gmail.com>
2019-07-02 00:59:52 +0300

    Transform App.css in App.scss

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import './components/main/Main';
+import AdressPanel from './components/adressPanel/AdressPanel';
 import Main from './components/main/Main';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App container">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
+        
+        <AdressPanel/>
 
         <Main/>
         
