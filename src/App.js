fda2a52381e433dd8e0c3a2e800a3b34a6c4668d
NovoseletskiyVadim <novoseletskiy.v@gmail.com>
2019-07-04 13:53:24 +0300

    Dedlete React header

--- src/App.js
+++ src/App.js
@@ -10,10 +10,7 @@ class App extends Component {
   render() {
     return (
       <div className="App container">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        
+       
         <AdressPanel/>
 
         <MobileMenu/>
